465b733274b26509711d33d9a61a88c3dd9b6d9f
René Schultz Madsen <rm@microting.dk>
2019-10-29 08:38:25 +0100

    Adding the new PluginGroupPermissionVersions.

--- Microting.AppointmentBase/Infrastructure/Data/AppointmentPnDbContext.cs
+++ Microting.AppointmentBase/Infrastructure/Data/AppointmentPnDbContext.cs
@@ -47,6 +47,7 @@ namespace Microting.AppointmentBase.Infrastructure.Data
         public DbSet<PluginConfigurationValueVersion> PluginConfigurationValueVersions { get; set; }
         public DbSet<PluginPermission> PluginPermissions { get; set; }
         public DbSet<PluginGroupPermission> PluginGroupPermissions { get; set; }
+        public DbSet<PluginGroupPermissionVersion> PluginGroupPermissionVersions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
